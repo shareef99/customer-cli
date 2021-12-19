93f5b01b4bea37dc8b055c91360faec82441ccb9
shareef <nadeemshareef934@gmail.com>
2021-12-19 17:45:41 +0530

    Alternative way for add

--- commands.js
+++ commands.js
@@ -1,14 +1,47 @@
 const program = require("commander");
 const { addCustomer, findCustomer } = require("./index");
+const { prompt } = require("inquirer");
+
+// Questions
+const questions = [
+    {
+        type: "input",
+        name: "firstName",
+        message: "What is the customer's first name?",
+    },
+    {
+        type: "input",
+        name: "lastName",
+        message: "What is the customer's last name?",
+    },
+    {
+        type: "input",
+        name: "phone",
+        message: "What is the customer's phone number?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the customer's email address?",
+    },
+];
 
 program.version("1.0.0").description("Customer CLI");
 
+// program
+//     .command("add <firstName> <lastName> <email> <phone>")
+//     .alias("a")
+//     .description("Add a new customer")
+//     .action((firstName, lastName, email, phone) => {
+//         addCustomer({ firstName, lastName, email, phone });
+//     });
+
 program
-    .command("add <firstName> <lastName> <email> <phone>")
+    .command("add")
     .alias("a")
     .description("Add a new customer")
-    .action((firstName, lastName, email, phone) => {
-        addCustomer({ firstName, lastName, email, phone });
+    .action(() => {
+        prompt(questions).then((answers) => addCustomer(answers));
     });
 
 program
